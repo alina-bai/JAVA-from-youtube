2b277e3b9e4ea10e67437935809d27543decb7be
Alina-bai <alina.baishukurova@gmail.com>
2024-10-06 12:45:13 +0400

    While loops in Java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,23 @@
-package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        String day = "Friday";
-        switch (day) {
-            case "Sunday": System.out.println("Its Sunday");
-            break;
-            case "Monday": System.out.println("Its Monday");
-            break;
-            case "Tuesday": System.out.println("Its Tuesday");
-            break;
-            case "Wednesday": System.out.println("Its Wednesday");
-            break;
-            case "Thursday": System.out.println("Its Thursday");
-            break;
-            case "Friday": System.out.println("Its Friday");
-            break;
-            case "Saturday": System.out.println("Its Saturday");
-            break;
-            default: System.out.println("That is not a day!");
+       import java.util.Scanner;
 
+        public class Main {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                int number;
+
+                System.out.println("Enter a positive number (negative to stop):");
+
+                while (true) { // Infinite loop
+                    number = scanner.nextInt(); // Read user input
+
+                    if (number < 0) {
+                        break; // Exit the loop if a negative number is entered
+                    }
+
+                    System.out.println("You entered: " + number);
+                }
+
+                System.out.println("Loop ended because you entered a negative number.");
+            }
         }
-    }
-}
